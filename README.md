c81ac2f89c6b47c8b17a454205e979efb0fcc486
ZBNB <354939531@qq.com>
2017-06-28 17:58:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Canvas whiteboard
+This is an example of canvas accessible (and editable) by multiple users.
+
+This time I wanted to learn something about sockets, namely [Socket.Io](http://socket.io) framework,
+hence I did the backend using it together with Express.
+
+## Running
+If you (for some reason) want to run this, make sure you have Node installed. Then:
+* Clone this repo with `git clone`
+* Run `npm install` from project's root
+* Once npm finishes, run `node app.js`
+* Open your browser, go to `http://localhost:3210`, and you shall see the whiteboard.
+Try opening several tabs pointing to that address, draw, and see what happens.
+
+Have fun!
---
+++ app.js
@@ -0,0 +1,62 @@
+/**
+ * Our app starts here.
+ */
+
+// Setup
+var express = require('express');
+var app = express();
+var server = app.listen(3210, function () {
+    console.log('Listening on port %d', server.address().port);
+});
+var io = require('socket.io')(server);
+
+// Return canvas.html for whatever request we get.
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/canvas.html');
+});
+
+// We will store image data here, so users that
+// connect after startup receive the current
+// state of the image.
+var imageData;
+
+// Color palette by Skyblue2u: http://www.colourlovers.com/palette/580974/Adrift_in_Dreams
+// Nice one!
+var colors = [ "#CFF09E", "#A8DBA8", "#79BD9A", "#3B8686", "#0B486B" ];
+
+var i = 0;
+// Setup emitters for each connection
+io.sockets.on('connection', function (socket) {
+
+    // Each new user gets a different color
+    if (i == 5) i = 0;
+
+    socket.on('create', function(room) {
+        console.log(room);
+        socket.room = room;
+        socket.join(room);
+        //get existed image as soon as join room
+        io.sockets.in(socket.room).emit('setup', colors[i++], imageData);
+
+    });
+
+    // socket.emit('setup', colors[i++], imageData);
+
+    // Broadcast all draw clicks.
+    socket.on('do-the-draw', function (data) {
+        socket.broadcast.to(socket.room).emit('draw', data);
+        imageData = data.imageData;
+    });
+
+    // ...and clear clicks as well
+    socket.on('clear', function () {
+        socket.broadcast.emit('clear');
+        imageData = null;
+    });
+
+    // Users modified image, let's save it
+    socket.on('save-data', function (data) {
+        imageData = data;
+    });
+});
+
